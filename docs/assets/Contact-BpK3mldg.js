3ddd0bfb2ff7dbb857703a16bfc73eb1af4b477e
Rasheed Amer <rasheedamer@Rasheeds-MacBook-Air.local>
2026-05-20 15:44:40 +0200

    Add proper cross-browser favicons (ico, 16/32/48/180/512px PNG, webmanifest)

--- docs/assets/Contact-BpK3mldg.js
+++ docs/assets/Contact-BpK3mldg.js
@@ -1,4 +1,4 @@
-import{c as i,j as e,u as l,r as d,M as m,I as h,a as x,F as p,S as u}from"./index-BxGdKmaG.js";import{A as g}from"./arrow-up-right-VAMvHLQN.js";/**
+import{c as i,j as e,u as l,r as d,M as m,I as h,a as x,F as p,S as u}from"./index-ByEMxoGk.js";import{A as g}from"./arrow-up-right-C6QATc5k.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- docs/assets/arrow-up-right-C6QATc5k.js
+++ docs/assets/arrow-up-right-C6QATc5k.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-BxGdKmaG.js";/**
+import{c as o}from"./index-ByEMxoGk.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
