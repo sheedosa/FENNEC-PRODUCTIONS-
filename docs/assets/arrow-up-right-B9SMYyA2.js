a8c2652cbb579a1a49807807d347725e640b62bc
Rasheed Amer <rasheedamer@Rasheeds-MacBook-Air.local>
2026-05-27 02:19:30 +0200

    Rebuild docs with adaptive favicon HTML

--- docs/assets/arrow-up-right-B9SMYyA2.js
+++ docs/assets/arrow-up-right-B9SMYyA2.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-ByEMxoGk.js";/**
+import{c as o}from"./index-DoT3AJDF.js";/**
  * @license lucide-react v0.546.0 - ISC
  *
  * This source code is licensed under the ISC license.
